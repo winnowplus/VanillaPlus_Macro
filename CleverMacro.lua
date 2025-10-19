3371f1379f2e89c1d3b019fbeedf1498f584d9c7
winnowplus <winnowplus@gmail.com>
2025-10-19 09:28:26 +0800

    workaround

--- CleverMacro.lua
+++ CleverMacro.lua
@@ -1,113 +1,3 @@
-local SPELL_COST_PATTERN        = "(%d+)%s*(%S+)";
-local SPELL_CACHE               = {};
-
-local SpellMixin		        = {};
-
-
-function SpellMixin:Init(slot, bookType)
-    self.slot = slot;
-    self.bookType = bookType;
-    self.texture = GetSpellTexture(slot, bookType);
-    self.name, self.rank = GetSpellName(slot, bookType);
-
-    if(self.name ~= nil) then
-        self.fullname = (self.rank == nil or self.rank == "") and self.name or self.name .. "(" .. self.rank .. ")";
-    end
-end
-
-function SpellMixin:GetCooldown()
-    return GetSpellCooldown(self.slot, self.bookType);
-end
-
-function SpellMixin:GetCost()
-    MacroTooltip:SetOwner(WorldFrame, "ANCHOR_NONE");
-    MacroTooltip:ClearLines();
-    MacroTooltip:SetSpell(self.slot, self.bookType);
-
-    local textLeft2 = MacroTooltipTextLeft2 and MacroTooltipTextLeft2:IsShown() and MacroTooltipTextLeft2:GetText() or nil;
-
-    if(textLeft2 ~= nil) then
-        local _, _, cost, powerTypeString = string.find(textLeft2, SPELL_COST_PATTERN);
-
-        return cost, powerTypeString;
-    end
-end
-
-local function GetPlayerSpellCahce()
-    local bookType = BOOKTYPE_SPELL;
-
-    if(SPELL_CACHE[bookType] == nil) then
-        SPELL_CACHE[bookType] = {};
-
-        for tabIndex = 1, GetNumSpellTabs() do
-            local _, _, offset, numSpells = GetSpellTabInfo(tabIndex);
-
-            for slot = offset + 1, offset + numSpells do
-                local spell = CreateAndInitFromMixin1(SpellMixin, slot, bookType);
-
-                if(spell.name ~= nil) then
-                    SPELL_CACHE[bookType][spell.name] = spell;
-                    SPELL_CACHE[bookType][spell.fullname] = spell;
-                end
-            end
-        end
-    end
-
-    return SPELL_CACHE[bookType];
-end
-
-local function GetPetSpellCahce()
-    local bookType = BOOKTYPE_PET;
-
-    if(SPELL_CACHE[bookType] == nil) then
-        SPELL_CACHE[bookType] = {};
-
-        local slot, spell = 0, nil;
-
-        repeat
-            slot = slot + 1;
-            spell = CreateAndInitFromMixin1(SpellMixin, slot, bookType);
-
-            if(spell.name ~= nil) then
-                SPELL_CACHE[bookType][spell.name] = spell;
-                SPELL_CACHE[bookType][spell.fullname] = spell;
-            end
-        until(spell.name == nil)
-    end
-
-    return SPELL_CACHE[bookType];
-end
-
-local function GetSpell(spellName, bookType)
-    if(bookType == nil) then
-        return GetPlayerSpellCahce()[spellName] or GetPetSpellCahce()[spellName];
-    elseif(bookType == BOOKTYPE_SPELL) then
-        return GetPlayerSpellCahce()[spellName];
-    elseif(bookType == BOOKTYPE_PET) then
-        return GetPetSpellCahce()[spellName];
-    end
-end
-
-
-local function ON_LEARNED_SPELL_IN_TAB()
-    SPELL_CACHE[BOOKTYPE_SPELL] = nil;
-end
-
-local function ON_PLAYER_PET_CHANGED()
-    SPELL_CACHE[BOOKTYPE_PET] = nil;
-end
-
-local function ON_UNIT_PET()
-    if(arg1 == "player") then
-        SPELL_CACHE[BOOKTYPE_PET] = nil;
-    end
-end
-
-MacroEventRegistry:RegisterFrameEventAndCallback("LEARNED_SPELL_IN_TAB", ON_LEARNED_SPELL_IN_TAB);
-MacroEventRegistry:RegisterFrameEventAndCallback("PLAYER_PET_CHANGED", ON_PLAYER_PET_CHANGED);
-MacroEventRegistry:RegisterFrameEventAndCallback("UNIT_PET", ON_UNIT_PET);
-
-
 --------------------------------------------------------------------------------
 -- CleverMacro v1.3.1 by _brain    VERSION = 1.7                              --
 --------------------------------------------------------------------------------
@@ -314,7 +204,7 @@ end
 
 --获取技能CD
 local function GetSpellCooldownByName(spellName)
-    local spell = GetSpell(spellName);
+    local spell = GetSpell111(spellName);
 
     if(spell ~= nil) then
         local _, duration = spell:GetCooldown()
@@ -407,7 +297,7 @@ local function GetSpellInfo(spellSlot)
 end
 
 local function GetSpellSlotByName(name)
-    local spell = GetSpell(name, "spell");
+    local spell = GetSpell111(name, "spell");
 
     return spell and spell.slot or nil;
 end
